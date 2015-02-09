4545648fbbda9e3a4cc6d12a5987d4918d76ee3e
marcwright <marc.wright@gmail.com>
2015-02-08 22:50:48 -0500

    Removing database stuff

--- app/controllers/wordclouds_controller.rb
+++ app/controllers/wordclouds_controller.rb
@@ -2,7 +2,7 @@ class WordcloudsController < ApplicationController
   def index
     @result = Wordcloud.cloud(params[:input])
   #   @result = "http://www.craigslist.org"
-    @song = Song.last
+    
 
   end
 
